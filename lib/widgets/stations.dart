ff7f27b0b80220cc878c9e3b7f5070e2f1d816e6
Semedy <abdisamed2023@hotmail.com>
2022-04-16 09:41:13 +0300

    Json

--- lib/widgets/stations.dart
+++ lib/widgets/stations.dart
@@ -1,4 +1,9 @@
+
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Station extends StatefulWidget {
   const Station({ Key? key }) : super(key: key);
@@ -8,8 +13,17 @@ class Station extends StatefulWidget {
 }
 
 class _StationState extends State<Station> {
+  getStationData() async{
+    var header = "maps.googleapis.com";
+    var resturl = "maps/api/place/nearbysearch/json?keyword=gas%20station&location=38.734802%2C35.467987&radius=1500&type=gas&key=AIzaSyBaCYVfyzEp9ZBQoSMYKXE41UB97zktNGo";
+    var response = await http.get(Uri.https(header, resturl));
+    var encodeFirst = json.encode(response.body);
+    var jsonData = jsonDecode(encodeFirst);
+    print(jsonData);
+  }
   @override
   Widget build(BuildContext context) {
+    getStationData();
     return Card(
       color: Colors.transparent,
       child: ListTile(
