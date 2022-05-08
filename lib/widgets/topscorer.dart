fe77d54d57d840c3c91b86a4f5100e718aa38403
abdulaziz028 <abdulaziz.musse@agu.edu.tr>
2022-05-08 23:11:43 +0300

    can be navigated from the main screen

--- lib/widgets/topscorer.dart
+++ lib/widgets/topscorer.dart
@@ -11,19 +11,21 @@ class ScoreTable extends StatefulWidget {
 class _ScoreTableState extends State<ScoreTable> {
   @override
   Widget build(BuildContext context) {
-    return  Container(
-                height: 30,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white)
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      child: Text(widget.name, style: TextStyle(color: Colors.white),),),
-                    Text(widget.score, style: TextStyle(color: Colors.white))
-                  ],
-                ),
-              );
+    return Container(
+      height: 30,
+      decoration: BoxDecoration(border: Border.all(color: Colors.white)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            child: Text(
+              widget.name,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Text(widget.score, style: TextStyle(color: Colors.white))
+        ],
+      ),
+    );
   }
 }
