26f42117a43b789dc46df116b37c4fe78f75bfa6
abdulaziz028 <abdulaziz.musse@agu.edu.tr>
2022-05-08 23:10:04 +0300

    Top Updater Screen

--- lib/topScreem.dart
+++ lib/topScreem.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:la_gasolina/widgets/topscorer.dart';
 
 class TopScorer extends StatefulWidget {
-  const TopScorer({ Key? key }) : super(key: key);
+  const TopScorer({Key? key}) : super(key: key);
 
   @override
   State<TopScorer> createState() => _TopScorerState();
@@ -17,56 +17,56 @@ class _TopScorerState extends State<TopScorer> {
         title: Text("Top Updaters"),
         backgroundColor: Color.fromARGB(255, 163, 162, 162),
       ),
-
       body: Column(
         children: [
-           Center(
-                    child: Container(
-                      margin: EdgeInsets.only(bottom: 20),
-                      child: CircleAvatar(
-                        radius: 35,
-                        backgroundColor: Colors.transparent,
-                        backgroundImage: AssetImage("assets/logo.png"),
-                      ),
-                    ),
-                  ),
-                  Center(
-                      child: Text(
-                    "La Gasolina",
-                    style: TextStyle(color: Colors.white, fontSize: 28),
-                  )),
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(bottom: 20),
+              child: CircleAvatar(
+                radius: 35,
+                backgroundColor: Colors.transparent,
+                backgroundImage: AssetImage("assets/logo.png"),
+              ),
+            ),
+          ),
+          Center(
+              child: Text(
+            "La Gasolina",
+            style: TextStyle(color: Colors.white, fontSize: 28),
+          )),
           Expanded(
             child: Container(
               margin: EdgeInsets.all(8),
               child: ListView(
                 children: [
                   Container(
-                    
-                   height: 30,
-                   decoration: BoxDecoration(
-                     color: Colors.grey,
-                     border: Border.all(color: Colors.white)
-                   ),
-                   child: Row(
-                     mainAxisAlignment: MainAxisAlignment.spaceAround,
-                     children: [
-                      
-                        Text("Name", style:  TextStyle(color: Colors.white),),
-                       Text("Score", style:  TextStyle(color: Colors.white))
-                     ],
-                   ),
-                      ),
-                     ScoreTable(name: "Aziz", score: "10",),
-                     ScoreTable(name: "Semedy", score: "8"),
-                     ScoreTable(name: "Abdurahim", score: "7")
+                    height: 30,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        border: Border.all(color: Colors.white)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          "Name",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text("Score", style: TextStyle(color: Colors.white))
+                      ],
+                    ),
+                  ),
+                  ScoreTable(
+                    name: "Aziz",
+                    score: "10",
+                  ),
+                  ScoreTable(name: "Semedy", score: "8"),
+                  ScoreTable(name: "Abdurahim", score: "7")
                 ],
               ),
             ),
           ),
         ],
       ),
-       
-      
     );
   }
 }
