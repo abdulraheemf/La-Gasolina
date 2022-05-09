9f2924be94ac26d7efbf0af527c36d69496ea931
abdulaziz028 <abdulaziz.musse@agu.edu.tr>
2022-05-09 14:21:21 +0300

    updaters

--- build/app/generated/source/buildConfig/debug/com/example/la_gasolina/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/example/la_gasolina/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.la_gasolina;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.la_gasolina";
+  public static final String APPLICATION_ID = "io.launchclub.lagasolina";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
