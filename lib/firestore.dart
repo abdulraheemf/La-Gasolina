03ac8c731e06da1fe6574e2efbcecccd776a4102
baris <baris.ince@agu.edu.tr>
2022-05-20 20:25:59 +0300

    db update

---
+++ lib/firestore.dart
@@ -0,0 +1,13 @@
+db = FirebaseFirestore.instance;
+
+final user = <String, dynamic>{
+  "Diesel": "25",
+  "Petroleum": "30",
+
+};
+
+db.collection("users").add(user).then((DocumentReference doc) =>
+print('DocumentSnapshot added with ID: ${doc.id}')) {
+  // TODO: implement then
+  throw UnimplementedError();
+}
