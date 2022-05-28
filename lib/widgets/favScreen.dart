a27eeb27c6fb4195d202d73dc68407f461b8aa88
abdulaziz028 <abdulaziz.musse@agu.edu.tr>
2022-05-28 13:03:33 +0300

    full functionality

--- lib/widgets/favScreen.dart
+++ lib/widgets/favScreen.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:la_gasolina/widgets/favStationCard.dart';
+
+import '../model/station.dart';
 
 class FavScreen extends StatefulWidget {
   const FavScreen({ Key? key }) : super(key: key);
@@ -8,7 +14,34 @@ class FavScreen extends StatefulWidget {
 }
 
 class _FavScreenState extends State<FavScreen> {
-  
+  List<Stations> favStations=[];
+  Future<void> fetchfavs() async{
+    final url =Uri.parse("https://la-gasolina-60017-default-rtdb.firebaseio.com/favs.json");
+    try{
+    final response = await http.get(url);
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+
+     final List<Stations> LoadedList = [];
+    extractedData.forEach((key, value) {
+     LoadedList.add(Stations(
+       name: value["name"], 
+       vicinity: value["vicinity"], 
+     ));
+    });
+    setState(() {
+       favStations=LoadedList;
+    });
+ 
+    }catch(error){
+      
+    }
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchfavs();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +50,26 @@ class _FavScreenState extends State<FavScreen> {
         backgroundColor: Colors.grey,
         title: Center(child: Text("Favorites")),
       ),
-
+      body: Container(
+                          decoration: BoxDecoration(
+                              color: Color(0xff4B4848),
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(12),
+                                  topRight: Radius.circular(12),
+                                  bottomRight: Radius.circular(12),
+                                  bottomLeft: Radius.circular(12))),
+                          margin: EdgeInsets.only(top: 0, left: 5, right: 5),
+                          padding: EdgeInsets.all(1),
+                          
+                          child: ListView(
+                            children: [
+                              ...favStations
+                                  .map(
+                                    (e) => Station(
+                                        name: e.name, vicinity: e.vicinity),
+                                  )
+                                  .toList(),
+                            ])),
     );
   }
 }
---
+++ lib/widgets/favStationCard.dart
@@ -0,0 +1,42 @@
+
+
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:la_gasolina/model/station.dart';
+import 'package:la_gasolina/widgets/gas_station_information.dart';
+import 'package:la_gasolina/widgets/wrapper.dart';
+
+class Station extends StatefulWidget {
+   String name, vicinity; 
+  Station({required this.name,  required this.vicinity});
+
+  @override
+  State<Station> createState() => _StationState();
+}
+
+class _StationState extends State<Station> {
+ 
+  @override
+  Widget build(BuildContext context) {
+    
+   
+    return Card(
+      color: Colors.transparent,
+      child: ListTile(
+                        leading: Icon(Icons.local_gas_station),
+                        title: Text(widget.name, style: TextStyle(color: Colors.white),),
+                        subtitle: Text(widget.vicinity, style: TextStyle(color: Colors.white)),
+                        trailing: Icon(Icons.favorite, color: Colors.red,),
+        onTap: () {Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Wrapper(name: widget.name)),
+        );
+
+        },
+                      ),
+    );
+  }
+}
